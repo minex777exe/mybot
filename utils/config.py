f88a2cd1382e5ef5d7ff919c28fb88e5ffddd5a1
minex777exe <minex777exe@gmail.com>
2026-02-15 02:38:33 +0530

    files

---
+++ utils/config.py
@@ -0,0 +1,9 @@
+import os
+
+TOKEN = os.environ.get("TOKEN")
+NAME = "Fire Bot"
+server = "https://discord.gg/XpTDJcAjtA"
+ch = "https://discord.com/channels/1452946542951665747/1452948479323148299"
+OWNER_IDS = [1090081155669311488]
+BotName = "Fire Bot"
+serverLink = "https://discord.gg/XpTDJcAjtA"
